ea058686e5d5e8bc3bb5eefd213247aeb34af777
Alan Mendelevich <ailon@ailon.org>
2020-05-21 14:17:27 +0300

    add DataField selectors for TreeMap data fields

--- src/components/modules/design/editors/DataFieldEditor.tsx
+++ src/components/modules/design/editors/DataFieldEditor.tsx
@@ -3,6 +3,7 @@ import { observer } from 'mobx-react';
 import { HTMLSelect, IOptionProps } from '@blueprintjs/core';
 import IPropertyEditorProps from '../IPropertyEditorProps';
 import { computed } from 'mobx';
+import { IChartData } from '../../../core/IChartData';
 
 @observer
 class DataFieldEditor extends Component<IPropertyEditorProps> {
@@ -12,12 +13,27 @@ class DataFieldEditor extends Component<IPropertyEditorProps> {
       this.props.editorState.chartData !== undefined &&
       this.props.editorState.chartData.length > 0
     ) {
-      Object.keys(this.props.editorState.chartData[0]).forEach(field => {
-        result.push({ label: field, value: field });
-      });
+      result.push(...this.getDataFields(this.props.editorState.chartData[0]));
     }
+    // return deduped
+    return result.filter(
+      (el, index, self) =>
+        self.findIndex(dup => dup.value === el.value) === index
+    );
+  }
+
+  private getDataFields(level: IChartData): IOptionProps[] {
+    const result: IOptionProps[] = [];
+    Object.keys(level).forEach(field => {
+      result.push({ label: field, value: field });
+      if (Array.isArray(level[field]) && level[field].length > 0) {
+        result.push(...this.getDataFields(level[field][0]));
+      }
+    });
+
     return result;
   }
+
   public render() {
     const p = this.props.property;
     if (this.dataFields !== undefined) {
