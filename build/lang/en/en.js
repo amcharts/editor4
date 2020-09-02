bd77ff7ed061c33a91c51b4ad16fae37ee1ca2a9
Alan (GitHub Action) <alan@amcharts.com>
2020-09-02 09:14:16 +0000

    add build

---
+++ build/lang/en/en.js
@@ -0,0 +1,9 @@
+import ui from './ui';
+import common from './common';
+import properties from './properties';
+
+export default {
+  ui: ui,
+  common: common,
+  properties: properties
+};
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fb95fc2d493e567201cbc00e937ec20f.js"
+  "./precache-manifest.807d4b633ede771782a4bb33e78bb3b3.js"
 );
 
 self.addEventListener('message', (event) => {
