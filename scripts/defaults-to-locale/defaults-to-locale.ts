5f650a850d354de4730e480e1f63b88e643dc57c
Alan Mendelevich <ailon@ailon.org>
2020-09-09 11:16:22 +0300

    fix excess custom property localizations were created

--- scripts/defaults-to-locale/defaults-to-locale.ts
+++ scripts/defaults-to-locale/defaults-to-locale.ts
@@ -49,9 +49,6 @@ function getAutoDisplayName(name: string): string {
 
 Object.keys(allDefaults).forEach(className => {
   const cls = allDefaults[className];
-  propertyTranslations[cls.name] = cls.displayName
-    ? cls.displayName
-    : getAutoDisplayName(cls.name);
   if (cls.properties !== undefined) {
     cls.properties.forEach(prop => {
       propertyCounts[prop.name] = Object.keys(propertyCounts).includes(
@@ -60,7 +57,18 @@ Object.keys(allDefaults).forEach(className => {
         ? propertyCounts[prop.name] + 1
         : 1;
     });
+  }
+});
+Object.keys(allDefaults).forEach(className => {
+  const cls = allDefaults[className];
+  propertyTranslations[cls.name] = cls.displayName
+    ? cls.displayName
+    : getAutoDisplayName(cls.name);
+  if (cls.properties !== undefined) {
     cls.properties.forEach(prop => {
+      if (cls.name === 'IAxisTickProperties' && prop.name === 'align') {
+        console.log(`IAxisTickProperties.align: ${propertyCounts[prop.name]}`);
+      }
       const translation = prop.displayName
         ? prop.displayName
         : getAutoDisplayName(prop.name);
